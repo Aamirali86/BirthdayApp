11d9a19ad5046efc5e38871e8b38a2ef127e0b25
Muhammad Aamir <aamir-muhammad@hotmail.com>
2020-12-13 21:25:30 +0500

    added date formatter for date of birth

--- BirthdayApp/Scenes/List/Cell/BirthdayCellViewModel.swift
+++ BirthdayApp/Scenes/List/Cell/BirthdayCellViewModel.swift
@@ -16,9 +16,20 @@ final class BirthdayCellViewModel: BirthdayCellViewModelType {
     //MARK:- Properties
     
     private let people: People
+    private let inputFormatter: DateFormatter
+    private let outputFormatter: DateFormatter
     
     init(people: People) {
         self.people = people
+        inputFormatter = DateFormatter()
+        inputFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'"
+        inputFormatter.locale = Locale.current
+        inputFormatter.timeZone = TimeZone.current
+        
+        outputFormatter = DateFormatter()
+        outputFormatter.dateFormat = "yyyy-MM-dd"
+        outputFormatter.locale = Locale.current
+        outputFormatter.timeZone = TimeZone.current
     }
     
     var name: String {
@@ -26,6 +37,7 @@ final class BirthdayCellViewModel: BirthdayCellViewModelType {
     }
     
     var date: String {
-        "\(people.dateOfBirth.date)"
+        guard let date = inputFormatter.date(from: people.dateOfBirth.date) else { return "" }
+        return outputFormatter.string(from: date)
     }
 }
